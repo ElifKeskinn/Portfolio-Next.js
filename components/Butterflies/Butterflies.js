49b85160c613f1ad659b2092987c0b3453d52ef2
Elif <elif.keskin233@gmail.com>
2024-12-12 23:44:13 +0300

    deployment 2

--- components/Butterflies/Butterflies.js
+++ components/Butterflies/Butterflies.js
@@ -1,20 +1,38 @@
 import { useState, useEffect } from 'react';
 import styles from './Butterflies.module.css';
-import Image from 'next/image';
 
 const Butterflies = () => {
+  // Farklı başlangıç pozisyonları ve animasyon süreleri için kelebekler
+  const butterflies = [
+    { id: 1, delay: '0s', size: 40 },
+    { id: 2, delay: '5s', size: 40 },
+    { id: 3, delay: '2s', size: 40 },
+    { id: 4, delay: '7s', size: 40 },
+    { id: 5, delay: '4s', size: 40 }
+  ];
+
   return (
     <div className={styles.butterfliesContainer}>
-      <div className={styles.butterfly}>
-        <Image 
-          src="/images/butterfly-flying.gif"
-          alt="Flying Butterfly"
-          width={50}
-          height={50}
-          className={styles.butterflyImage}
-          unoptimized
-        />
-      </div>
+      {butterflies.map((butterfly) => (
+        <div 
+          key={butterfly.id}
+          className={styles.butterfly}
+          style={{ 
+            animationDelay: butterfly.delay,
+            top: `${Math.random() * 100}%`  // Rastgele başlangıç pozisyonu
+          }}
+        >
+          <img 
+            src="/images/butterfly-flying.gif"
+            alt="Flying Butterfly"
+            style={{
+              width: `${butterfly.size}px`,
+              height: `${butterfly.size}px`
+            }}
+            className={styles.butterflyImage}
+          />
+        </div>
+      ))}
     </div>
   );
 };
